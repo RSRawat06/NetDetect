20187b16f6e1da0079e94a90e296517996d22763
Eric Zhao <eric@whimmly.com>
2017-05-25 18:57:42 -0700

    Small changes to git commit, config, and movement of install_venv.sh script

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-PROJ_ROOT = '/home/em/projects/smart/'
+PROJ_ROOT = '/home/em/projects/botnet_pred/'
 DATASET_URL = PROJ_ROOT + "models/all_features.csv"
 
 FRAMEWORK = "tensorflow"
@@ -8,4 +8,4 @@ N_HIDDEN = 5
 BATCH_S = 11
 N_FEATURES = 13
 ITERATIONS = 5
-N_TEST = 100
+N_TEST = 5000
--- install_venv.sh
+++
@@ -1,4 +0,0 @@
-py_v=$(which python)
-virtualenv -p $py_v venv --no-site-packages
-source venv/bin/activate
-pip install -r requirements.txt
