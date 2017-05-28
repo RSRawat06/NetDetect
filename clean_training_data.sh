c4c974b7da1a6c1472e0cd10dc39014df6733eb4
Eric Zhao <eric@whimmly.com>
2017-05-28 13:16:27 -0700

    Modified '.git's, added ISOT data handling

--- clean_training_data.sh
+++ clean_training_data.sh
@@ -1,2 +1,2 @@
-# rm data/data.csv data/modified_data.csv models/sorted_flow.csv models/all_features.csv
-echo "Success: Training data files, including those used to generate it, have been deleted"
+rm data/data.csv data/modified_data.csv models/sorted_flow.csv
+echo "Success: Training data file generation intermediaries, have been deleted"
