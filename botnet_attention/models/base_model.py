4d4f76adcb40a5332f4d1d2dfe0fbbc2bedbf204
Eric Zhao <eric@whimmly.com>
2017-09-11 12:01:51 -0700

    Updated base_model and universal model configs

--- botnet_attention/models/base_model.py
+++ botnet_attention/models/base_model.py
@@ -16,29 +16,33 @@ class Base_Model():
       self.saver = tf.train.Saver(tf.global_variables())
     self.saver.save(self.sess, model_config.MODEL_DIR + model_config.VANILLA_MODEL_NAME)
 
-  def load(self, model_url):
+  def load(self):
     if self.saver is None:
       self.saver = tf.train.Saver(tf.global_variables())
     self.saver.restore(self.sess, model_config.MODEL_DIR + model_config.VANILLA_MODEL_NAME)
 
-  def train(self, training_data, testing_data):
+  def initialize(self):
+    self.var_init = tf.global_variables_initializer()
     self.var_init.run()
+
+  def train(self, training_data, testing_data):
+    # self.var_init.run()
     for j in range(self.config.ITERATIONS):
       start_time = time.time()
 
-      for i in range(-1, self.config.N_BATCHES, len(training_data['targets'])):
+      for i in range(0, training_data['targets'].shape[0], self.config.BATCH_SIZE):
         feed_dict = {
-            self.x: training_data['X'][i:i + self.config.N_BATCHES],
-            self.target: training_data['Y'][i:i + self.config.N_BATCHES]
+            self.x: training_data['X'][i:i + self.config.BATCH_SIZE],
+            self.target: training_data['Y'][i:i + self.config.BATCH_SIZE]
         }
         __, train_loss, train_acc = self.sess.run([self.optim, self.loss, self.acc], feed_dict=feed_dict)
         print("Train loss: ", train_loss, "\nTrain acc on train: ", train_acc)
 
       total_testing_acc = []
-      for i in range(-1, self.config.N_BATCHES, len(testing_data['targets'])):
+      for i in range(0, testing_data['targets'].shape[0], self.config.BATCH_SIZE):
         feed_dict = {
-            self.x: testing_data['X'][i:i + self.config.N_BATCHES],
-            self.target: testing_data['Y'][i:i + self.config.N_BATCHES]
+            self.x: testing_data['X'][i:i + self.config.BATCH_SIZE],
+            self.target: testing_data['Y'][i:i + self.config.BATCH_SIZE]
         }
         testing_acc = self.sess.run([self.acc], feed_dict=feed_dict)
         total_testing_acc.append(testing_acc)
@@ -48,11 +52,11 @@ class Base_Model():
       print("Iteration", j, "took: ", elapsed_time)
 
   def predict(self, input_data):
-    self.var_init.run()
+    # self.var_init.run()
     all_predictions = []
-    for i in range(-1, self.config.N_BATCHES, len(input_data)):
+    for i in range(0, input_data.shape[0], self.config.BATCH_SIZE):
       feed_dict = {
-          self.x: input_data[i:i + self.config.N_BATCHES],
+          self.x: input_data[i:i + self.config.BATCH_SIZE],
       }
       predictions = self.sess.run([self.prediction], feed_dict=feed_dict)
       all_predictions += list(predictions)
--- botnet_attention/models/config.py
+++ botnet_attention/models/config.py
@@ -5,35 +5,10 @@ Configuration file for models module.
 """
 
 import os
-DATA_DIR = os.path.dirname(os.path.realpath(__file__)) + "/dumps/"
+MODEL_DIR = os.path.dirname(os.path.realpath(__file__)) + "/dumps/"
 
 VANILLA_MODEL_NAME = "vanilla.model"
 VANILLA_META_NAME = "vanilla_tf.meta"
 
 SELF_MODEL_NAME = "self.model"
 SELF_META_NAME = "self_tf.meta"
-
-FRAMEWORK = "tensorflow"
-MAX_FLOW_LENGTH = 40
-MAX_FLOW_SEQUENCE_LENGTH = 40
-
-MODEL_CONFIG = {
-    "basic": [
-        100,
-        [1, 5],
-        50,
-        MAX_FLOW_SEQUENCE_LENGTH,
-        MAX_FLOW_LENGTH,
-        13
-    ],
-    "packets": [
-        20,
-        20,
-        4
-    ],
-    "flows": [
-        20,
-        20,
-        4
-    ]
-}
