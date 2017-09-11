889cf826a49b2ae6aee7216b73c74d5d2ffb3c52
Eric Zhao <eric@whimmly.com>
2017-09-11 12:27:47 -0700

    Midway through patching self_attention model

--- botnet_attention/models/self_attention.py
+++ botnet_attention/models/self_attention.py
@@ -5,34 +5,36 @@ tf.logging.set_verbosity(tf.logging.ERROR)
 
 
 class Self_Attention(Base_Model):
+  def __init__(self, sess, config, model_name="self_attention.model"):
+    Base_Model.__init__(self, sess, config)
+    self.model_name = model_name
+
   def build_model(self):
     # Set initial vars
-    self.x = tf.placeholder(tf.float32, [self.config.N_BATCHES, self.config.N_FLOWS, self.config.N_PACKETS, self.config.N_FEATURES])
-    self.target = tf.placeholder(tf.float32, [self.config.N_BATCHES, 1], name="target")
+    self.x = tf.placeholder(tf.float32, [self.config.BATCH_SIZE, self.config.N_FLOWS, self.config.N_PACKETS, self.config.N_FEATURES])
+    self.target = tf.placeholder(tf.float32, [self.config.BATCH_SIZE, 1], name="target")
 
     ##############################
 
     self.fwd_gru_p = tf.nn.rnn_cell.GRUCell(self.config.N_PACKET_GRU_HIDDEN)
     self.bwd_gru_p = tf.nn.rnn_cell.GRUCell(self.config.N_PACKET_GRU_HIDDEN)
 
-    self.x_flatten = tf.reshape(self.x, (self.config.N_BATCHES * self.config.N_FLOWS, self.config.N_PACKETS, self.config.N_FEATURES))
-    self.H_p_flat, _, _ = tf.nn.static_bidirectional_rnn(self.fwd_gru_p, self.bwd_gru_p, tf.unstack(self.x_flatten), dtype=tf.float32)
-    assert(self.H_p_flat.shape == (self.config.N_BATCHES * self.config.N_FLOWS, self.config.N_PACKETS, 2 * self.config.N_PACKET_GRU_HIDDEN))
-    self.H_p = tf.reshape(self.H_p_flat, (self.config.N_BATCHES, self.config.N_FLOWS, self.config.N_PACKETS, -1))
-    assert(self.H_p.shape == (self.config.N_BATCHES, self.config.N_FLOWS, self.config.N_PACKETS, 2 * self.config.N_PACKET_GRU_HIDDEN))
+    self.x_unstacked = tf.unstack(tf.transpose(tf.reshape(self.x, (self.config.BATCH_SIZE * self.config.N_FLOWS, self.config.N_PACKETS, self.config.N_FEATURES)), (1, 0, 2)), name="x_unstacked")
+    self.H_p, _, _ = tf.nn.static_bidirectional_rnn(self.fwd_gru_p, self.bwd_gru_p, self.x_unstacked, dtype=tf.float32, scope="packet_rnn")
+    assert(tf.stack(self.H_p).shape == (self.config.BATCH_SIZE * self.config.N_FLOWS, self.config.N_PACKETS, 1 * self.config.N_PACKET_GRU_HIDDEN))
 
     self.W_s_1_p = tf.get_variable("W_s_1_p", shape=[self.config.N_PACKET_ATTENTION_HIDDEN, 2 * self.config.N_PACKET_GRU_HIDDEN])
     self.W_s_2_p = tf.get_variable("W_s_2_p", shape=[self.config.N_PACKET_ATTENTION_HIDDEN])
 
-    self.r_p = tf.nn.softmax(tf.matmul(self.W_s_2_p, tf.tanh(tf.matmul(self.W_s_1_p, tf.tranpose(self.H_p, (0, 1, 3, 2))))))
-    assert(self.r_p.shape == (self.config.N_BATCHES, self.config.N_FLOWS, self.config.N_PACKETS))
+    self.r_p = tf.nn.softmax(tf.matmul(self.W_s_2_p, tf.tanh(tf.matmul(self.W_s_1_p, tf.tranpose(self.H_p, (0, 2, 1))))))
+    assert(self.r_p.shape == (self.config.BATCH_SIZE, self.config.N_FLOWS, self.config.N_PACKETS))
 
-    self.M_p = tf.squeeze(tf.matmul(tf.transpose(self.H_p, (0, 1, 3, 2)), tf.expand_dims(self.r_p, 3)))
-    assert(self.M_p.shape == (self.config.N_BATCHES, self.config.N_FLOWS, 2 * self.config.N_PACKET_GRU_HIDDEN))
+    self.M_p = tf.squeeze(tf.matmul(tf.transpose(self.H_p, (0, 2, 1)), tf.expand_dims(self.r_p, 3)))
+    assert(self.M_p.shape == (self.config.BATCH_SIZE, self.config.N_FLOWS, 2 * self.config.N_PACKET_GRU_HIDDEN))
 
     self.W_p = tf.get_variable("W_p", shape=[2 * self.config.N_PACKET_GRU_HIDDEN, self.config.N_PACKET_DENSE_HIDDEN])
     self.A_p = tf.tanh(tf.matmul(self.M_p, self.W_p))
-    assert(self.A_p.shape == (self.config.N_BATCHES, self.config.N_FLOWS, self.config.N_PACKET_DENSE_HIDDEN))
+    assert(self.A_p.shape == (self.config.BATCH_SIZE, self.config.N_FLOWS, self.config.N_PACKET_DENSE_HIDDEN))
 
     ##############################
 
@@ -40,26 +42,26 @@ class Self_Attention(Base_Model):
     self.bwd_gru_f = tf.nn.rnn_cell.GRUCell(self.config.N_FLOW_GRU_HIDDEN)
 
     self.H_f, _, _ = tf.nn.static_bidirectional_rnn(self.fwd_gru_f, self.bwd_gru_f, self.A_p)
-    assert(self.H_f.shape == (self.config.N_BATCHES, self.config.N_FLOWS, 2 * self.config.N_FLOW_GRU_HIDDEN))
+    assert(self.H_f.shape == (self.config.BATCH_SIZE, self.config.N_FLOWS, 2 * self.config.N_FLOW_GRU_HIDDEN))
 
     self.W_s_1_f = tf.get_variable("W_s_1_f", shape=[self.config.N_FLOW_ATTENTION_HIDDEN, 2 * self.config.N_FLOW_GRU_HIDDEN])
     self.W_s_2_f = tf.get_variable("W_s_2_f", shape=[self.config.N_FLOW_ATTENTION_HIDDEN])
 
     self.r_f = tf.nn.softmax(tf.matmul(self.W_s_2_f, tf.tanh(tf.matmul(self.W_s_1_f, tf.tranpose(self.H_f, (0, 2, 1))))))
-    assert(self.r_f.shape == (self.config.N_BATCHES, self.config.N_FLOWS))
+    assert(self.r_f.shape == (self.config.BATCH_SIZE, self.config.N_FLOWS))
 
     self.M_f = tf.squeeze(tf.matmul(tf.transpose(self.H_f, (0, 2, 1)), tf.expand_dims(self.r, 2)))
-    assert(self.M_f.shape == (self.config.N_BATCHES, 2 * self.config.N_FLOW_GRU_HIDDEN))
+    assert(self.M_f.shape == (self.config.BATCH_SIZE, 2 * self.config.N_FLOW_GRU_HIDDEN))
 
     self.W_f = tf.get_variable("W_f", shape=[2 * self.config.N_FLOW_GRU_HIDDEN, self.config.N_FLOW_DENSE_HIDDEN])
     self.A_f = tf.tanh(tf.matmul(self.M_f, self.W_f))
-    assert(self.A_f.shape == (self.config.N_BATCHES, self.config.N_FLOW_DENSE_HIDDEN))
+    assert(self.A_f.shape == (self.config.BATCH_SIZE, self.config.N_FLOW_DENSE_HIDDEN))
 
     ##############################
 
     self.W_final = tf.get_variable("W_final", shape=[self.config.N_FLOW_DENSE_HIDDEN, 2])
     self.prediction = tf.nn.softmax(tf.matmul(self.A_f, self.W_final))
-    assert(self.prediction.shape == (self.config.N_BATCHES, 2))
+    assert(self.prediction.shape == (self.config.BATCH_SIZE, 2))
 
     self.loss = -tf.reduce_sum(self.target * tf.log(self.prediction) * tf.constant(self.config.RESULT_WEIGHTING))
 
--- botnet_attention/tests/models/test_self_attention.py
+++ botnet_attention/tests/models/test_self_attention.py
@@ -5,10 +5,20 @@ import numpy as np
 
 
 def test_self_attention():
+  tf.reset_default_graph()
   with tf.Session() as sess:
     model = models.self_attention.Self_Attention(sess, config)
     model.build_model()
-    model.predict(np.zeros((config.N_BATCHES, config.N_FLOWS, config.N_PACKETS, config.N_FEATURES)))
+    model.initialize()
+    predictions = model.predict(np.full((2 * config.BATCH_SIZE, config.N_FLOWS, config.N_PACKETS, config.N_FEATURES), 10))
+    model.save()
+
+  tf.reset_default_graph()
+  with tf.Session() as sess:
+    model2 = models.self_attention.Self_Attention(sess, config)
+    model2.build_model()
+    model2.load()
+    predictions = model2.predict(np.full((2 * config.BATCH_SIZE, config.N_FLOWS, config.N_PACKETS, config.N_FEATURES), 10))
 
 
 if __name__ == "__main__":
