70ddc5add5c5c64da434b6a8d3465b877472e4f1
Eric Zhao <ericlzhao28@gmail.com>
2017-09-18 14:41:12 -0700

    Finished all but main preprocessing tests

--- botnet_attention/tests/preprocessing/test_featurize.py
+++ botnet_attention/tests/preprocessing/test_featurize.py
@@ -1,67 +1,73 @@
 from ...preprocessing import featurize
+import numpy as np
+from . import config
 
+headers_key = {0: 'size', 1: 'width', 2: 'type', 3: 'flow_id', 4: 'ip', 5: 'ip2', 6: 'proto', 7: 'port'}
 
-def test_featurize_row():
-  store_categoricals = preprocess.create_store_categoricals(["proto"], ["type"])
-  parse_feature = preprocess.create_parse_feature(["size", "width"], ["type"], ["proto"], ["ip", "ip2"], 'flow_id')
+raw_first = [323, '23', 'apple', 5.0, 4, 6, 'chocolate:dank:shit', 10]
+raw_second = [22.5, '14', 'pear', 9.0, 4, 8, 'chocolate:weird:blah', 2900]
+raw_third = [12.5, '14', 'chocolate', 9.0, 4, 8, 'chocolate:weird:shit', 1998]
+raw_fourth = [22.5, '14', 'pear', 9.0, 4, 8, 'chocolate:weird', 29000]
 
-  headers_key = {0: "size", 1: "width", 2: "type", 3: "flow_id", 4: "ip", 5: "ip2", 6: "proto"}
-  raw_first = [323, '23', 'apple', 5.0, 4, 6, "chocolate:dank:shit"]
-  raw_second = [22.5, '14', 'pear', 9.0, 4, 8, "chocolate:dank"]
-  records = {'categorical': {}, 'protocol': {}}
-  for row in [raw_first, raw_second]:
-    for i, value in enumerate(row):
-      field = headers_key[i]
-      records = store_categoricals(field, value, records)
+meta_first = {'flow_id': 5, 'participants': [{"score": 0, "ip": 4}, {"score": 1, "ip": 6}]}
+meta_second = {'flow_id': 9, 'participants': [{"score": 0, "ip": 4}, {"score": 1, "ip": 8}]}
+meta_third = {'flow_id': 9, 'participants': [{"score": 0, "ip": 4}, {"score": 1, "ip": 8}]}
+meta_fourth = {'flow_id': 9, 'participants': [{"score": 0, "ip": 4}, {"score": 1, "ip": 8}]}
 
-  clean_first = ([323.0, 23.0, 1, 0, 1, 1, 1], 5, ['4', '6'])
-  clean_second = ([22.5, 14.0, 0, 1, 1, 1, 0], 9, ['4', '8'])
+clean_first = [323.0, 23.0, 1, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0]
+clean_second = [22.5, 14.0, 0, 1, 0, 1, 0, 0, 1, 1, 0, 1, 0]
+clean_third = [12.5, 14.0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1]
+clean_fourth = [22.5, 14.0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0]
 
-  assert(clean_first == preprocess.parse_row(raw_first, headers_key, parse_feature, records))
-  assert(clean_second == preprocess.parse_row(raw_second, headers_key, parse_feature, records))
-
-
-def test_metadatize_row():
-  pass
+flow_field = 'flow_id'
+numerical_fields = ['size', 'width']
+protocol_fields = ['proto']
+categorical_fields = ['type']
+port_fields = ['port']
+participant_fields = ['ip', 'ip2']
+malicious_ips = [6, 8]
 
 
 def test_store_categoricals():
-  store_categoricals = preprocess.create_store_categoricals(["proto"], ["type"])
-  headers_key = {0: "size", 1: "width", 2: "type", 3: "flow_id", 4: "ip", 5: "ip2", 6: "proto"}
-  raw_first = [323, '23', 'apple', 5.0, 4, 6, "chocolate:dank:shit"]
-  raw_second = [22.5, '14', 'pear', 9.0, 4, 8, "chocolate:weird:blah"]
-  records = {'categorical': {}, 'protocol': {}}
+  all_records = {'categorical': {}, 'protocol': {}, 'port': {}}
   for row in [raw_first, raw_second]:
-    for i, value in enumerate(row):
-      field = headers_key[i]
-      records = store_categoricals(field, value, records)
-  assert(records == {"categorical": {"type": ["apple", "pear"]}, "protocol": {"proto": ["chocolate", "dank", "shit", "weird", "blah"]}})
+    all_records = featurize.store_categoricals(row, headers_key, all_records, 'proto', 'type', 'port')
+  assert(all_records == {'categorical': {'type': ['apple', 'pear']}, 'protocol': {'proto': ['chocolate', 'dank', 'shit', 'weird', 'blah']}, 'port': {'port': [10, 200]}})
+
 
 def test_store_categorical_over():
-  store_categoricals = preprocess.create_store_categoricals(["proto"], ["type"], 2)
-  headers_key = {0: "size", 1: "width", 2: "type", 3: "flow_id", 4: "ip", 5: "ip2", 6: "proto"}
-  raw_first = [323, '23', 'apple', 5.0, 4, 6, "chocolate:dank:shit"]
-  raw_second = [22.5, '14', 'pear', 9.0, 4, 8, "chocolate:weird:blah"]
-  raw_third = [22.5, '14', 'chocolate', 9.0, 4, 8, "chocolate:weird:shit"]
-  records = {'categorical': {}, 'protocol': {}}
-  for j, row in enumerate([raw_first, raw_second, raw_third]):
-    for i, value in enumerate(row):
-      field = headers_key[i]
-      if j == 2 and field == "type":
-        failed = False
-        try:
-          records = store_categoricals(field, value, records)
-        except ValueError:
-          failed = True
-        assert(failed)
-        continue
-      records = store_categoricals(field, value, records)
-  assert(records == {"categorical": {"type": ["apple", "pear", "chocolate"]}, "protocol": {"proto": ["chocolate", "dank", "shit", "weird", "blah"]}})
+  all_records = {'categorical': {}, 'protocol': {}, 'port': {}}
+  for i, row in enumerate([raw_first, raw_second, raw_third]):
+    if i == 2:
+      failed = False
+      try:
+        all_records = featurize.store_categoricals(row, headers_key, all_records, 'proto', 'type', 'port', 5, 5)
+      except ValueError:
+        failed = True
+      assert(failed)
+      continue
+  assert(all_records == {'categorical': {'type': ['apple', 'pear']}, 'protocol': {'proto': ['chocolate', 'dank', 'shit', 'weird', 'blah']}, 'port': {'port': [10, 200]}})
+
+
+def test_featurize_row():
+  all_records = {'categorical': {}, 'protocol': {}, 'port': {}}
+  for row in [raw_first, raw_second, raw_third, raw_fourth]:
+    all_records = featurize.store_categoricals(row, headers_key, all_records, 'proto', 'type', 'port')
+  for clean, raw in ((clean_first, raw_first), (clean_second, raw_second), (clean_third, raw_third), (clean_fourth, raw_fourth)):
+    assert(clean == featurize.featurize_row(raw, headers_key, all_records, numerical_fields, protocol_fields, categorical_fields, port_fields))
+
+
+def test_metadatize_row():
+  for meta, raw in ((meta_first, raw_first), (meta_second, raw_second), (meta_third, raw_third), (meta_fourth, raw_fourth)):
+    assert(meta == featurize.metadatize_row(raw, headers_key, malicious_ips, flow_field, participant_fields))
+
 
 def test_metadatize_csv():
-  pass
+  metadata = featurize.metadatize_csv(config.DATA_DIR + "sample_csv.csv", malicious_ips, flow_field, participant_fields)
+  assert(metadata == [meta_first, meta_second, meta_third, meta_fourth])
 
 
 def test_featurize_csv():
-  pass
+  X = featurize.featurize_csv(config.DATA_DIR + "sample_csv.csv", numerical_fields, protocol_fields, categorical_fields, port_fields)
+  assert(np.all(X == np.array([clean_first, clean_second, clean_third, clean_fourth])))
 
--- botnet_attention/tests/preprocessing/test_utils.py
+++ botnet_attention/tests/preprocessing/test_utils.py
@@ -1,6 +1,7 @@
 from ...preprocessing import utils
 
+
 def test_build_headers():
   row = ["chocolate", "yummy", "weird"]
   headers_key = utils.build_headers(row)
-  assert(headers_key = {1: "chocolate", 2: "yummy", 3: "weird"})
+  assert(headers_key == {1: "chocolate", 2: "yummy", 3: "weird"})
