4bbfe7f0e8adf532bc60a8fb5b0b3d5a4ffe64b3
Eric Zhao <ericlzhao28@gmail.com>
2017-11-02 07:59:35 -0700

    Bug fixes for ISCX pipeline refactoring

--- NetDetect/datasets/iscx/load.py
+++ NetDetect/datasets/iscx/load.py
@@ -1,16 +1,18 @@
 from . import config
 from .logger import set_logger
+from ..utils import shaping_utils
 import pickle
 
 
-def load():
+def load(n_test, n_val):
   '''
   Loads preprocessed data dump if possible.
   '''
   try:
     set_logger.info("Dataset exists. Attempting pickle load...")
     with open(config.DUMPS_DIR + config.PROCESSED_SAVE_NAME, "rb") as f:
-      return pickle.load(f)
+      X, Y = shaping_utils.shuffle_twins(pickle.load(f))
+      return shaping_utils.partition_dataset(X, Y, n_test, n_val)
 
   except (EOFError, OSError, IOError) as e:
     set_logger.info("Dataset does not exist. Returning None.")
