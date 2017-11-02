2d4e63961ea352f54aee52b8341f9a5a50e32606
Eric Zhao <ericlzhao28@gmail.com>
2017-11-02 07:37:24 -0700

    Removal of old generic dataset tests and models.

--- NetDetect/src/models/__init__.py
+++ NetDetect/src/models/__init__.py
@@ -1,5 +1,2 @@
-from .sequential_model import SequentialModel
-from .flat_model import FlatModel
-from .double_seq_model import DoubleSeqModel
 from .flow_model import FlowModel
 
--- NetDetect/tests/models/run_tensorboard.sh
+++
@@ -1,1 +0,0 @@
-tensorboard --logdir double_seq_train:/NetDetect/tests/models/saves/double_seq/graphs,flat_train:/NetDetect/tests/models/saves/flat/graphs,sequential_train:/NetDetect/tests/models/saves/sequential/graphs
