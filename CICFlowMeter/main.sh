17955f140241012d5310569b2ffbd324e7e6041e
Eric Zhao <ericlzhao28@gmail.com>
2017-11-14 20:28:57 -0800

    Removed main req, added .gitignore to CICFlow

--- CICFlowMeter/main.sh
+++ CICFlowMeter/main.sh
@@ -1,2 +1,1 @@
-bash /CICFlowMeter/utils/split_pcap.sh /CICFlowMeter/input/iscx.pcap /CICFlowMeter/input/output.pcap
-java -Xmx3g -XX:-UseGCOverheadLimit -Djava.library.path=/CICFlowMeter/jnetpcap -jar /CICFlowMeter/CICFlowMeter/CICFlowMeter.jar /CICFlowMeter/input/ /CICFlowMeter/output/
+java -Xmx5g -XX:-UseGCOverheadLimit -Djava.library.path=/CICFlowMeter/jnetpcap -jar /CICFlowMeter/CICFlowMeter/CICFlowMeter.jar /CICFlowMeter/input/ /CICFlowMeter/output/
