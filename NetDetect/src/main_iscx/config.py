a0601b61092dfecdbc4f059e01ceb3f8af35b70b
Eric Zhao <ericlzhao28@gmail.com>
2017-11-18 17:45:26 -0800

    Simplified main_iscx train

--- NetDetect/src/main_iscx/config.py
+++ NetDetect/src/main_iscx/config.py
@@ -8,7 +8,6 @@ CHECKPOINTS_DIR = SAVES_DIR + "checkpoints/"
 
 BATCH_SIZE = 32
 TEST_SIZE = 4096
-# VAL_SIZE = 0
 ITERATIONS = 100
 N_FEATURES = 77
 N_STEPS = 22
@@ -16,7 +15,7 @@ LAYERS = {
     'h_gru': 64,
     'h_att': 16,
     'o_gru': 64,
-    'h_dense': 32,
+    'h_dense': 64,
     'o_dense': 32,
     'h_dense2': 32,
     'o_dense2': 16
@@ -24,6 +23,6 @@ LAYERS = {
 N_CLASSES = 2
 REGULARIZATION = 0.1
 
-REPORT_INTERVAL = 500
-SAVE_INTERVAL = 50000
+REPORT_INTERVAL = 200
+SAVE_INTERVAL = 600
 
--- NetDetect/src/main_iscx/train.py
+++ NetDetect/src/main_iscx/train.py
@@ -1,4 +1,4 @@
-from ...src.models import FlowModel, FlowAttModel
+from ...src.models import FlowAttModel
 from ...datasets.iscx import load
 from . import config
 from .logger import train_logger
@@ -7,7 +7,6 @@ import tensorflow as tf
 
 def train():
   with tf.Session() as sess:
-    # model = FlowModel(sess, config, train_logger)
     model = FlowAttModel(sess, config, train_logger)
     train_logger.info("Model created.")
 
@@ -20,10 +19,9 @@ def train():
         "\nN_steps: " + str(config.N_STEPS) +
         "\nLayers: " + str(config.LAYERS) +
         "\nn_classes: " + str(config.N_CLASSES) +
-        "\nregularization: " + str(config.REGULARIZATION) +
-        "\nreport interval: " + str(config.REPORT_INTERVAL) +
-        "\nsave interval: " + str(config.SAVE_INTERVAL) +
-        # "\nModel: flow_model"
+        "\nRegularization: " + str(config.REGULARIZATION) +
+        "\nReport interval: " + str(config.REPORT_INTERVAL) +
+        "\nSave interval: " + str(config.SAVE_INTERVAL) +
         "\nModel: flow_att_model"
     )
 
@@ -31,14 +29,12 @@ def train():
     train_logger.info("Dataset loaded.")
 
     def __epoch_evaluation(self, sub_epoch):
-      if (sub_epoch - 1) % config.SAVE_INTERVAL == 0:
-        print("Train eval")
-        loss, acc, tpr, fpr, summary = self.evaluate(
-            dataset[0][0][:4096], dataset[0][1][:4096], prefix="train")
-        self.logger.info(
-            "Epoch: %f has train loss: %f, train accuracy: %f, \
-             TPR: %s, FPR: %s" % (sub_epoch, loss, acc, str(tpr), str(fpr)))
-        self.train_writer.add_summary(summary, global_step=sub_epoch)
+      loss, acc, tpr, fpr, summary = self.evaluate(
+          dataset[0][0][:4096], dataset[0][1][:4096], prefix="train")
+      self.logger.info(
+          "Epoch: %f has train loss: %f, train accuracy: %f, \
+           TPR: %s, FPR: %s" % (sub_epoch, loss, acc, str(tpr), str(fpr)))
+      self.train_writer.add_summary(summary, global_step=sub_epoch)
 
       loss, acc, tpr, fpr, summary = self.evaluate(
           dataset[1][0], dataset[1][1], prefix="test")
