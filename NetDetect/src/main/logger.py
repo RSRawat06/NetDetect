f13eae9d805729eef7652123579c34aa0da1fdcd
Ubuntu <em@bigTest.ba5k2qpbedluliqptg4j2bgglh.xx.internal.cloudapp.net>
2017-11-24 11:06:04 +0000

    Deployment changes for multi-train

--- NetDetect/src/main/logger.py
+++ NetDetect/src/main/logger.py
@@ -2,6 +2,6 @@ from ...logs import setup_logger
 import logging
 
 
-train_logger = setup_logger('train', 'main/', logging.DEBUG)
+train_logger = setup_logger('train', 'main/', logging.INFO)
 eval_logger = setup_logger('eval', 'main/', logging.DEBUG)
 
